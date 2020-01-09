415304a049cf06a0baedc201b3ec95131d349235
Uros Zeravica <uros.zeravica@telenor.rs>
2020-01-09 08:27:21 +0100

    adding directory and shop reducers

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,14 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
-import CollectioPreview from "../../components/collection-preview/collection-preview";
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collection: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collection } = this.state;
-    return (
-      <div className="shop-page">
-        {collection.map(({ id, ...otherCollectionProps }) => (
-          <CollectioPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+import React from "react";
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
